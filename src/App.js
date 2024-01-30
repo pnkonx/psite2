a16ca4dbd0a6bd4ba7c36c371603ca36b1a77ebc
Nic <pnkonx@gmail.com>
2024-01-29 17:46:55 -0700

    app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function decrementCount() {
+    setCount(count - 1)
+  }
+  function incrementCount() {
+    setCount(count + 1)
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+        <div className="App">
+          <button onClick={decrementCount}>-</button>
+          <span>{count}</span>
+          <button onClick={incrementCount}>+</button>
+        </div>
   );
+        
+    
 }
 
 export default App;
